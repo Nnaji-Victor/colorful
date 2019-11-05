2717a41809390b4c06ff75ef38a93f8388a97b09
Nnaji Victor <nnajivictor0@gmail.com>
2019-11-05 11:00:33 +0100

    added correct route for different palettes

--- src/App.js
+++ src/App.js
@@ -4,14 +4,17 @@ import SeedColors from './components/SeedColors';
 import {generatePalette} from './components/ColorHelper';
 import {Route, Switch} from 'react-router-dom';
 
+const findPalette = (id) => SeedColors.find( palette => palette.id === id);
+
 function App() {
   return (
     <Switch>
       <Route exact path="/" render={() => <h1>Hello Palette</h1>} />
-      <Route exact path="/palette/:id" render={() => <h1>Hello individual Palette</h1>}/>
+  <Route exact path="/palette/:id" render={(routeProps) => <Palette palette={generatePalette(findPalette(routeProps.match.params.id))} />}/>
     </Switch>
     // <div className="App">
     //   <Palette palette={generatePalette(SeedColors[4])}/>
+    // <Palette palette={generatePalette(findPalette)}
     // </div>
   );
 }
